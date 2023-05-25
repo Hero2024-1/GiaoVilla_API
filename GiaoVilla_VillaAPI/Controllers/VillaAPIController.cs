096c06ed36cf54978927fcdfe30073720a6a1d3c
Giao Tran <giaotran2001lk@gmail.com>
2023-05-25 13:42:37 +0700

    create a first end point

---
+++ GiaoVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,38 @@
+﻿using GiaoVilla_VillaAPI.Data;
+using GiaoVilla_VillaAPI.Models;
+using GiaoVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiaoVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult<VillaDTO> GetVillas(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+    }
+}
